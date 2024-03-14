c650f69297779db52022d9102f681cab29c97bdc
enes selçuk <enesselcu25@gmail.com>
2024-03-14 14:12:16 +0300

    splash fix

--- app/src/main/java/com/enesselcuk/moviesui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/enesselcuk/moviesui/screens/splash/SplashScreen.kt
@@ -1,26 +1,14 @@
 package com.enesselcuk.moviesui.screens.splash
 
 import android.annotation.SuppressLint
-import android.content.Context
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.enesselcuk.moviesui.R
-import com.enesselcuk.moviesui.di.UserSettingsModule.UserSettingDataStore
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
+
 
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
@@ -28,19 +16,6 @@ fun SplashScreen(
     goHome: () -> Unit,
     goLogin: () -> Unit,
 ) {
-
-    val viewModel = hiltViewModel<SplashViewModel>()
-
-    UiObserver(goHome = { goHome.invoke() }, goLogin = { goLogin.invoke() })
-
-}
-
-@Composable
-fun UiObserver(
-    goHome: () -> Unit,
-    goLogin: () -> Unit
-) {
-
     val viewModel = hiltViewModel<SplashViewModel>()
 
     val preloaderLottieComposition by rememberLottieComposition(
@@ -66,6 +41,4 @@ fun UiObserver(
         composition = preloaderLottieComposition,
         progress = preloaderProgress,
     )
-
-
 }
